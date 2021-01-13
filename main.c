8b758ed0c3a0aea271cc186757e5005ab9419454
Kema-dev <jeremy.jdn@free.fr>
2021-01-13 12:47:45 +0100

    reviwed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:28:58 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/12/20 23:26:06 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/01/13 12:47:27 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	main(void)
 	size_t	nb_itt;
 
 	i = 0;
-	nb_itt = 2;
-	fd = open("./gnlTester/files/big_line_no_nl", O_RDONLY);
+	nb_itt = 20;
+	fd = open("./file", O_RDONLY);
 	line = malloc(sizeof(char **) * nb_itt);
 	while (i < nb_itt)
 	{
 		printf("out=%d ", get_next_line(fd, line));
 		printf("line=%s\n", *line);
-		printf("i=%ld\n", i);
+		printf("line=%ld\n", i+1);
 		free(*line);
 		line++;
 		i++;
